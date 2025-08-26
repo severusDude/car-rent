5cd6fe7e7d570c3346cd182186c62dfa0d8f3ce8
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2025-08-26 21:26:04 +0700

    feat(rentals): update create rental action

--- src/app/(rentals)/(create)/actions.ts
+++ src/app/(rentals)/(create)/actions.ts
@@ -1,9 +1,9 @@
 "use server";
 
 import { carService } from "@/services/car-service";
+import { promotionService } from "@/services/promotion-service";
 import { rentalService } from "@/services/rental-service";
 import { revalidatePath } from "next/cache";
-import { z } from "zod";
 
 export type ActionResult<T = any> = {
   success: boolean;
@@ -13,23 +13,102 @@ export type ActionResult<T = any> = {
 
 export async function createRental(formData: FormData): Promise<ActionResult> {
   try {
+    // extract form data
     const carId = Number(formData.get("carId"));
-    const tenantName = formData.get("tenantName") as string;
+    const customer = formData.get("customer") as string;
     const startDate = new Date(formData.get("startDate") as string);
-    const endDate = new Date(formData.get("endDate") as string);
+    const duration = Number(formData.get("duration"));
+    const extraHours = Number(formData.get("extraHours"));
 
-    const rental = await rentalService.create({
-      carId,
-      tenantName,
-      startDate,
-      endDate,
+    // Validate required fields
+    if (!carId || !customer || !startDate || !duration || duration <= 0) {
+      return {
+        success: false,
+        errors: "Missing required fields or invalid duration",
+      };
+    }
+
+    // Get car detail
+    const car = await carService.show(carId);
+    if (!car) {
+      return {
+        success: false,
+        errors: "Car not found",
+      };
+    }
+
+    // calculate payment base amount
+    const baseAmount = car.price * duration;
+
+    // calculate payment extra amount
+    const extraAmount = extraHours * (100 * 1000);
+
+    // find applicable promotion
+    const promotion = await promotionService.findBestApplicable(
+      duration,
+      baseAmount
+    );
+
+    // apply promotion
+    let discountAmount = 0;
+    let promotionId = null;
+
+    if (promotion) {
+      promotionId = promotion.id;
+
+      // apply discount
+      if (promotion.type === "PERCENTAGE") {
+        discountAmount = (baseAmount * promotion.value) / 100;
+      } else if (promotion.type === "FIXED") {
+        discountAmount = promotion.value;
+      }
+
+      // ensure discount not exceed price
+      if (discountAmount > baseAmount) {
+        discountAmount = baseAmount;
+      }
+    }
+
+    //calculate total amount
+    const totalAmount = baseAmount - discountAmount + extraAmount;
+
+    // // insert to db
+    // const rental = await rentalService.create({
+    //   car: { connect: { id: car.id } },
+    //   customer,
+    //   startDate,
+    //   duration,
+    //   extraHours,
+    // });
+
+    // initiate transaction with create rental and payment
+    const result = await rentalService.createWithPayment({
+      rental: {
+        carId,
+        customer,
+        startDate,
+        duration,
+        extraHours,
+      },
+      payment: {
+        baseAmount,
+        discountAmount,
+        extraAmount,
+        totalAmount,
+        promotionId,
+      },
     });
 
+    // refresh
     revalidatePath("/dashboard");
 
     return {
       success: true,
-      data: rental,
+      data: {
+        rental: result.rental,
+        payment: result.payment,
+        promotion: promotion,
+      },
     };
   } catch (error) {
     return {
--- src/services/rental-service.ts
+++ src/services/rental-service.ts
@@ -3,10 +3,19 @@ import { Prisma, Rental } from "@prisma/client";
 
 type CreateInput = {
   carId: number;
-  tenantName: string;
+  customer: string;
   startDate: Date;
-  endDate: Date;
+  duration: number;
+  extraHours: number;
+  payment: Prisma.RentalCreateInput;
 };
+
+export type RentalPayload = Prisma.RentalGetPayload<{
+  include: {
+    car: true;
+    payment: { include: { promotion: true } };
+  };
+}>;
 
 export class RentalService {
   /**
@@ -14,11 +23,12 @@ export class RentalService {
    *
    * @returns A list of all rentals.
    */
-  async index(): Promise<
-    Prisma.RentalGetPayload<{ include: { car: true } }>[]
-  > {
+  async index(): Promise<RentalPayload[]> {
     return await prisma.rental.findMany({
-      include: { car: true },
+      include: {
+        car: true,
+        payment: { include: { promotion: true } },
+      },
     });
   }
 
@@ -28,8 +38,14 @@ export class RentalService {
    * @param id The ID of the rental to retrieve.
    * @returns The rental with the given ID, or null if no such rental exists.
    */
-  async show(id: number): Promise<Rental | null> {
-    return await prisma.rental.findUnique({ where: { id } });
+  async show(id: number): Promise<RentalPayload | null> {
+    return await prisma.rental.findUnique({
+      where: { id },
+      include: {
+        car: true,
+        payment: { include: { promotion: true } },
+      },
+    });
   }
 
   /**
@@ -38,8 +54,68 @@ export class RentalService {
    * @param data The data for the rental to create.
    * @returns The created rental.
    */
-  async create(data: CreateInput): Promise<Rental> {
-    return await prisma.rental.create({ data });
+  async create(data: Prisma.RentalCreateInput): Promise<RentalPayload> {
+    return await prisma.rental.create({
+      data,
+      include: {
+        car: true,
+        payment: { include: { promotion: true } },
+      },
+    });
+  }
+
+  async createWithPayment({
+    rental,
+    payment,
+  }: {
+    rental: {
+      carId: number;
+      customer: string;
+      startDate: Date;
+      duration: number;
+      extraHours: number;
+    };
+    payment: {
+      baseAmount: number;
+      discountAmount: number;
+      extraAmount: number;
+      totalAmount: number;
+      promotionId?: number | null;
+    };
+  }) {
+    return await prisma.$transaction(async (tx) => {
+      //create rental first
+      const newRental = await tx.rental.create({
+        data: {
+          carId: rental.carId,
+          customer: rental.customer,
+          startDate: rental.startDate,
+          duration: rental.duration,
+          extraHours: rental.extraHours,
+        },
+        include: {
+          car: true,
+        },
+      });
+
+      // create payment
+      const newPayment = await tx.payment.create({
+        data: {
+          rentalId: newRental.id,
+          baseAmount: payment.baseAmount,
+          discountAmount: payment.discountAmount,
+          extraAmount: payment.extraAmount,
+          totalAmount: payment.totalAmount,
+          promotionId: payment.promotionId,
+          status: "PENDING",
+        },
+        include: {
+          promotion: true,
+        },
+      });
+
+      return { rental: newRental, payment: newPayment };
+    });
   }
 }
 
